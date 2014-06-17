9a8430939e12381edd8e95d6855a6b0085a96207
Kyle Oba <koba@mudphone.com>
2014-06-17 04:12:53 -1000

    Create basic grid as checkerboard pattern.

--- src/cljminecraft/agora/grid.clj
+++ src/cljminecraft/agora/grid.clj
@@ -1,5 +1,47 @@
-(ns cljminecraft.agora.grid)
+(ns cljminecraft.agora.grid
+  (:require
+   [cljminecraft.agora.block :as block]
+   [cljminecraft.agora.location :as location]))
+
+(def COLS 80)
+(def ROWS 40)
+(def SECTOR-W 1)
+(def GRID-BASE-H 1)
+
+(defn block-for-height
+  [h]
+  (condp > h
+    75 block/ICE
+    50 block/STONE
+    25 block/TNT
+    :else block/OBSIDIAN))
+
+(defn block-for-base
+  [c r]
+  (cond
+   (and (even? c) (even? r)) block/OBSIDIAN
+   (and (odd? c) (even? r)) block/STONE
+   (and (even? c) (odd? r)) block/STONE
+   :else block/OBSIDIAN))
+
+(defn make-sector
+  [mat base-loc magnitude]
+  (let [h (int magnitude)]
+    (for [dy (range h)
+          :let [loc (location/loc-plus base-loc {:y dy})]]
+      (block/make-block {:bkt-loc (:bkt-loc loc)
+                   :block-type mat}))))
 
 (defn make-grid
-  [{:keys [loc width height]}]
-  )
+  [{:keys [origin-loc cols rows]}]
+  (future
+    (for [c (range cols)
+          r (range rows)
+          :let [base-loc (location/loc-plus origin-loc
+                                            {:x c :z r})]]
+      (do
+        (make-sector (block-for-base c r)
+                     base-loc
+                     GRID-BASE-H)
+        (Thread/sleep 5)))
+    {:msg :done}))
--- src/cljminecraft/agora/location.clj
+++ src/cljminecraft/agora/location.clj
@@ -1,12 +1,30 @@
-(ns cljminecraft.agora.location)
+(ns cljminecraft.agora.location
+  (:import
+   [org.bukkit Location]))
+
+(defn bkt-loc->loc
+  [bkt-loc]
+  {:x       (.getX bkt-loc)
+   :y       (.getY bkt-loc)
+   :z       (.getZ bkt-loc)
+   :pitch   (.getPitch bkt-loc)
+   :yaw     (.getYaw bkt-loc)
+   :world   (.getWorld bkt-loc)
+   :bkt-loc bkt-loc})
 
 (defn location
   [thing]
-  (let [bk-loc (.getLocation thing)]
-    {:x       (.getX bk-loc)
-     :y       (.getY bk-loc)
-     :z       (.getZ bk-loc)
-     :pitch   (.getPitch bk-loc)
-     :yaw     (.getYaw bk-loc)
-     :world   (.getWorld bk-loc)
-     :bkt-bk-loc bk-loc}))
+  (let [bkt-loc (.getLocation thing)]
+    (bkt-loc->loc bkt-loc)))
+
+(defn bk-location-plus
+  [bkt-loc {:keys [x y z] :or {x 0 y 0 z 0}}]
+  (let [loc (bkt-loc->loc bkt-loc)]
+    (new Location (:world loc)
+         (+ x (:x loc))
+         (+ y (:y loc))
+         (+ z (:z loc)))))
+
+(defn loc-plus
+  [{:keys [bkt-loc]} delta]
+  (bkt-loc->loc (bk-location-plus bkt-loc delta)))
