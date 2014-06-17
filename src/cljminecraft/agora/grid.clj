a33e200c4a0874ab31c8ba8bdcf62a4b3c966a21
Kyle Oba <koba@mudphone.com>
2014-06-17 11:21:49 -1000

    Socket connection for grid construction works.

--- src/cljminecraft/agora/grid.clj
+++ src/cljminecraft/agora/grid.clj
@@ -1,22 +1,24 @@
 (ns cljminecraft.agora.grid
   (:require
    [cljminecraft.agora.block :as block]
-   [cljminecraft.agora.location :as location]))
+   [cljminecraft.agora.location :as location]
+   [cljminecraft.logging :as log]))
 
+(def MAX-H 100)
 (def COLS 80)
 (def ROWS 40)
 (def SECTOR-W 1)
 (def GRID-BASE-H 1)
 
-(defn block-for-height
+(defn material-for-height
   [h]
-  (condp > h
+  (condp < h
     75 block/ICE
     50 block/STONE
     25 block/TNT
-    :else block/OBSIDIAN))
+    block/OBSIDIAN))
 
-(defn block-for-base
+(defn material-for-base
   [c r]
   (cond
    (and (even? c) (even? r)) block/OBSIDIAN
@@ -24,24 +26,43 @@
    (and (even? c) (odd? r)) block/STONE
    :else block/OBSIDIAN))
 
-(defn make-sector
+(defn draw-sector
   [mat base-loc magnitude]
   (let [h (int magnitude)]
-    (for [dy (range h)
-          :let [loc (location/loc-plus base-loc {:y dy})]]
-      (block/make-block {:bkt-loc (:bkt-loc loc)
-                   :block-type mat}))))
+    (doall
+     (for [dy (range h)
+           :let [loc (location/loc-plus base-loc {:y dy})]]
+       (block/make-block {:bkt-loc (:bkt-loc loc)
+                          :block-type mat})))))
+
+(defn clear-sector
+  [base-loc]
+  (draw-sector block/AIR base-loc MAX-H))
+
+(defn make-sector
+  ([base-loc magnitude]
+     (make-sector (material-for-height (int magnitude)) base-loc magnitude))
+  ([mat base-loc magnitude]
+     (if-not (>= magnitude MAX-H)
+       (clear-sector base-loc))
+     (draw-sector mat base-loc magnitude)))
+
+(defn base-loc
+  [{:keys [origin-loc col row]}]
+  (location/loc-plus origin-loc
+                     {:x col :z row}))
 
 (defn make-grid
   [{:keys [origin-loc cols rows]}]
   (future
-    (for [c (range cols)
-          r (range rows)
-          :let [base-loc (location/loc-plus origin-loc
-                                            {:x c :z r})]]
-      (do
-        (make-sector (block-for-base c r)
-                     base-loc
-                     GRID-BASE-H)
-        (Thread/sleep 5)))
+    (dorun
+     (for [c (range cols)
+           r (range rows)
+           :let [base-loc (base-loc {:origin-loc origin-loc
+                                     :col c :row r})]]
+       (do
+         (make-sector (material-for-base c r)
+                      base-loc
+                      GRID-BASE-H)
+         (Thread/sleep 5))))
     {:msg :done}))
--- src/cljminecraft/agora/socket.clj
+++ src/cljminecraft/agora/socket.clj
@@ -1,7 +1,8 @@
 (ns cljminecraft.agora.socket
   (:require
+   [clojure.edn :as edn]
    [http.async.client :as h]
-
+   [cljminecraft.agora.grid :as grid]
    [cljminecraft.logging :as log]))
 
 (def WS_URI "ws://localhost:3000/agora-socket")
@@ -9,7 +10,7 @@
 (defonce client (atom nil))
 (defonce ws (atom nil))
 
-(def output-text (atom []))
+(defonce output-text (atom []))
 
 ;; { :name "datomic"
 ;;   :type :db-txn
@@ -21,23 +22,21 @@
 ;;      :grid-name "agora" }}
 
 (defn open-socket
-  []
-  (reset! client (h/create-client))
-  (reset! ws (h/websocket @client
-                          WS_URI
-                          :text (fn [con msg]
-                                  (log/info "ws text")
-                                  (log/info "conn: %s" con)
-                                  (log/info "msg: %s" msg)
-                                  (swap! output-text conj msg))
-                          :close (fn [con status]
-                                   (log/info "ws close")
-                                   (log/info "conn: %s" con)
-                                   (log/info "status: %s" status)
-                                   (swap! output-text conj "closed"))
-                          :open (fn [con]
-                                  (log/info "ws open")
-                                  (log/info "conn: %s" con)))))
+  ([]
+     (open-socket {}))
+  ([{:keys [text open close]}]
+     (reset! client (h/create-client))
+     (reset! ws (h/websocket @client
+                             WS_URI
+                             :text (or text
+                                       (fn [con msg]
+                                         (log/info "ws msg: %s" msg)))
+                             :close (or close
+                                        (fn [con status]
+                                          (log/info "ws close status: %s" status)))
+                             :open (or open
+                                       (fn [con]
+                                         (log/info "ws open")))))))
 
 (defn make-msg
   [{:keys [msg msg-type name]
@@ -54,7 +53,7 @@
 (defn send-chat-msg
   [msg]
   (if @ws
-    (send (make-msg msg))))
+    (send (make-msg {:msg msg}))))
 
 (defn enable-polling
   []
@@ -66,7 +65,34 @@
 
 (defn close-socket
   []
-  (h/close @ws)
-  (reset! ws nil)
-  (.close @client)
-  (reset! client nil))
+  (when @ws
+    (h/close @ws)
+    (reset! ws nil))
+  (when @client
+    (.close @client)
+    (reset! client nil)))
+
+(defn draw-mag
+  [grid-origin x y magnitude]
+  (let [loc (grid/base-loc {:origin-loc grid-origin :col x :row y})]
+    (grid/make-sector loc magnitude)))
+
+(defn handle-socket-msg
+  [grid-origin]
+  (fn [con socket-msg]
+    (let [{:keys [type msg name]} (edn/read-string socket-msg)]
+     (if (= type :db-txn)
+       (let [{:keys [x y magnitude]} msg]
+         (log/info "drawing magnitude: %f" magnitude)
+         (draw-mag grid-origin x y magnitude))))))
+
+(defn connect-to-agora
+  [grid-origin]
+  (grid/make-grid {:origin-loc grid-origin
+                   :cols grid/COLS
+                   :rows grid/ROWS})
+  (open-socket
+   {:text (handle-socket-msg grid-origin)})
+  (send-chat-msg "socket connected from minecraft")
+  (enable-polling)
+  (send-chat-msg "polling enabled"))
